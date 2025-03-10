959fda75581adf504126be6f395eec399043f4bd
Joy-kitet <debrajoykitet@gmail.com>
2025-03-10 14:42:48 -0700

    modified the files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return(
-    <h1>Welcome to our website!</h1>
-  )
-}
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+const fName = 'JOY';
+const lName = 'Kitet';
+const number = 10;
+ReactDOM.render(<div>
+  <h1>Hello, {fName + "" + lNamegit }</h1>
+  <h2>This are the Days of the week!</h2>
+  <p>Your lucky number is  {number}</p>
+  <ul>
+    
+    <li>Monday</li>
+    <li> Tuesday</li>
+    <li> Wednesday</li>  </ul>
+  
+  </div>, document.getElementById('root'));
