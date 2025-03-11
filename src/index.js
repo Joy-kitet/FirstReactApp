8b256707074d247351c84d80aa250e9d76820075
Joy-kitet <debrajoykitet@gmail.com>
2025-03-10 17:09:46 -0700

    modified

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Use 'react-dom/client' for React 18+ 
 
 const fName = 'JOY';
 const lName = 'Kitet';
 const number = 10;
-ReactDOM.render(<div>
-  <h1>Hello, {fName + "" + lNamegit }</h1>
-  <h2>This are the Days of the week!</h2>
-  <p>Your lucky number is  {number}</p>
-  <ul>
-    
-    <li>Monday</li>
-    <li> Tuesday</li>
-    <li> Wednesday</li>  </ul>
-  
-  </div>, document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <div>
+    <h1>Hello, {fName + " " + lName}</h1>
+    <h2>These are the Days of the week!</h2>
+    <p>Your lucky number is {number}</p>
+    <ul>
+      <li>Monday</li>
+      <li>Tuesday</li>
+      <li>Wednesday</li>
+    </ul>
+  </div>
+);
